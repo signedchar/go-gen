93f32ac079ca4b1a6a80c5bb288afe8b566f9df1
Erbeth Charte <echarte@tutanota.com>
2022-03-08 11:38:02 -0400

    Largest palindrome product Source refactor to POO.

--- large_palindrome_product/main.go
+++ large_palindrome_product/main.go
@@ -5,108 +5,105 @@ import (
 	"strconv"
 )
 
-type palindrome struct {
-	prd [2]int
-	r   int
+type palindrom struct {
+	m   int
+	n   int
+	pal string
 }
 
-// Verify if "len(str)" is equal to "lng"
-func pal_str_p(str string, lng int) bool {
-	var lbool bool
+type lpalindrom struct {
+	lstpal []palindrom
+}
 
-	if len(str) == lng {
-		lbool = true
+func reverse(s string) string {
+	r := ""
+
+	for _, v := range s {
+		r = string(v) + r
+	}
+
+	return r
+}
+
+func (p palindrom) even_word_p() bool {
+	if len(p.pal)%2 == 0 {
+		return true
 	} else {
-		lbool = false
+		return false
 	}
-
-	return lbool
 }
 
-// Verify if the string of length == 6 is a palindrome.
-func pal6(str string) bool {
-	var lbool bool
-	
-	if pal_str_p(str, 6) {
-		if str[0:3] == (str[5:6] + str[4:5] + str[3:4]) {
-			lbool = true
-		} else {
-			lbool = false
-		}
+func (p palindrom) h_tail_pal() [2]string {
+	var h_pal, tail_pal string
+	var r [2]string
+	lng := len(p.pal)
+
+	if p.even_word_p() {
+		hlf := lng / 2
+		h_pal = p.pal[0:hlf]
+		tail_pal = reverse(p.pal[hlf:lng])
+		r = [2]string{h_pal, tail_pal}
+	} else {
+		hlf := (lng - 1) / 2
+		h_pal = p.pal[0:hlf]
+		tail_pal = reverse(p.pal[hlf+1 : lng])
+		r = [2]string{h_pal, tail_pal}
 	}
 
-	return lbool
+	return r
 }
 
-// Verify if the string of length == 5 is a palindrome.
-func pal5(str string) bool {
-	var lbool bool
-
-	if pal_str_p(str, 5) {
-		if str[0:2] == (str[4:5] + str[3:4]) {
-			lbool = true
-		} else {
-			lbool = false
-		}
+func (p palindrom) palindrom_p() bool {
+	pal := p.h_tail_pal()
+	if pal[0] == pal[1] {
+		return true
+	} else {
+		return false
 	}
-
-	return lbool
 }
 
-func pals() []palindrome {
-	var lpals []palindrome
-	var ij [2]int
-	var pal_tmp palindrome
+func (lp lpalindrom) palindromes(start, stop int) lpalindrom {
+	// spal == single palindrome
+	var spal palindrom
 
-	for i := 999; i >= 100; i-- {
-		for j := 999; j >= 100; j-- {
-			str := strconv.Itoa(i * j)
-
-			if pal6(str) {
-				//string to integer == stoi
-				stoi, _ := strconv.Atoi(str)
-				ij = [2]int{i, j}
-				pal_tmp.prd = ij
-				pal_tmp.r = stoi
-				lpals = append(lpals, pal_tmp)
-				break
-			}
-
-			if pal5(str) {
-				stoi, _ := strconv.Atoi(str)
-				ij = [2]int{i, j}
-				pal_tmp.prd = ij
-				pal_tmp.r = stoi
-				lpals = append(lpals, pal_tmp)
+	for i := start; i >= stop; i-- {
+		for j := start; j >= stop; j-- {
+			r := i * j
+			spal.pal = strconv.Itoa(r)
+			if spal.palindrom_p() {
+				spal.m = i
+				spal.n = j
+				lp.lstpal = append(lp.lstpal, spal)
 				break
 			}
 		}
 	}
 
-	return lpals
+	return lp
 }
 
-func maxl(lst []palindrome) palindrome {
-	var pal_tmp palindrome
-	max_tmp := lst[0].r
+func (lp lpalindrom) max() palindrom {
+	var pal_tmp palindrom
+	var r int
 
-	for i := 1; i < len(lst); i++ {
-		if max_tmp < lst[i].r {
-			max_tmp = lst[i].r
-			pal_tmp = lst[i]
+	r, _ = strconv.Atoi(pal_tmp.pal)
+
+	for i := 1; i < len(lp.lstpal); i++ {
+		lr, _ := strconv.Atoi(lp.lstpal[i].pal)
+		if r < lr {
+			r = lr
+			pal_tmp = lp.lstpal[i]
 		} else {
 			continue
 		}
-		
 	}
 
 	return pal_tmp
 }
-   
+
 func main() {
-	palindromes := pals()
-	pal := maxl(palindromes)
-	x := pal.prd[0]
-	y := pal.prd[1]
-	fmt.Printf("The largest palindrome made from the product of 3 digits numbers is: %d = %d x %d\n", pal.r, x, y)
+	var lp lpalindrom
+	palindroms := lp.palindromes(999, 100)
+	pal_max := palindroms.max()
+	fmt.Printf("The largest palindrome made from the product of 3 digits numbers is: %d x %d = %s", pal_max.m, pal_max.n, pal_max.pal)
 }
