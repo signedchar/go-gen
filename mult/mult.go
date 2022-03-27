391e7a55b0be6b8fde2e8bdac5627603d6dd51bd
Erbeth Charte <echarte@tutanota.com>
2022-03-27 19:15:01 -0400

    Clean the mult3 and mult5 specific cases of mult(div, x int) bool.

--- mult/mult.go
+++ mult/mult.go
@@ -1,29 +1,5 @@
 package mult
 
-func mult3(x int) bool {
-	var i bool
-	
-	if x % 3 == 0 {
-		i = true
-	} else {
-		i = false
-	}
-
-	return i
-}
-
-func mult5(x int) bool {
-	var i bool
-
-	if x % 5 == 0 {
-		i = true
-	} else {
-		i = false
-	}
-
-	return i
-}
-
 func mult(div, x int) bool {
 	var i bool
 
