552998ea584fdace9331cdc90de9746c50a74c2d
Erbeth Charte <echarte@tutanota.com>
2022-03-27 19:36:56 -0400

    Adding smult(limit int) int This function sum the numbers multiples of 3 and 5.

--- mult/mult.go
+++ mult/mult.go
@@ -1,13 +1,25 @@
 package mult
 
-func mult(div, x int) bool {
+func mult(num, div int) bool {
 	var i bool
 
-	if div % x == 0 {
+	if num % div == 0 {
 		i = true
 	} else {
 		i = false
 	}
 
 	return i
+}
+
+func smult(limit int) int {
+	var count int
+	
+	for i := 1; i < limit; i++ {
+		if mult(i,3) || mult(i,5) {
+			count += i
+		}
+	}
+
+	return count
 }
